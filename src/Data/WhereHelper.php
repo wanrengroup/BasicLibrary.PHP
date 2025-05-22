85b67c011191870646b66c3b4dfc7b54d5fd8437
解大劦 <9727005@qq.com>
2025-05-22 10:35:10 +0800

    feat: 为Where条件生成器，添加可以在集合类型的字段中查找某个项目的功能

--- src/Data/WhereHelper.php
+++ src/Data/WhereHelper.php
@@ -16,66 +16,66 @@ namespace WanRen\Data;
  */
 class WhereHelper
 {
-    public static function Equal(string $field, $value): array
+    public static function Equal(string $fieldName, $value): array
     {
-        return array($field, "=", $value);
+        return array($fieldName, "=", $value);
     }
 
-    public static function NotEqual(string $field, $value): array
+    public static function NotEqual(string $fieldName, $value): array
     {
-        return array($field, "<>", $value);
+        return array($fieldName, "<>", $value);
     }
 
-    public static function GreaterThan(string $field, $value): array
+    public static function GreaterThan(string $fieldName, $value): array
     {
-        return array($field, ">", $value);
+        return array($fieldName, ">", $value);
     }
 
-    public static function GreaterThanOrEqual(string $field, $value): array
+    public static function GreaterThanOrEqual(string $fieldName, $value): array
     {
-        return array($field, ">=", $value);
+        return array($fieldName, ">=", $value);
     }
 
-    public static function LessThan(string $field, $value): array
+    public static function LessThan(string $fieldName, $value): array
     {
-        return array($field, "<", $value);
+        return array($fieldName, "<", $value);
     }
 
-    public static function LessThanOrEqual(string $field, $value): array
+    public static function LessThanOrEqual(string $fieldName, $value): array
     {
-        return array($field, "<=", $value);
+        return array($fieldName, "<=", $value);
     }
 
     /**
      *
-     * @param string $field
+     * @param string $fieldName
      * @param array|string $values 数组或者用逗号分隔的字符串
      * @return array
      */
-    public static function In(string $field, array|string $values): array
+    public static function In(string $fieldName, array|string $values): array
     {
-        return array($field, "IN", $values);
+        return array($fieldName, "IN", $values);
     }
 
     /**
      *
-     * @param string $field
+     * @param string $fieldName
      * @param array|string $values 数组或者用逗号分隔的字符串
      * @return array
      */
-    public static function NotIn(string $field, array|string $values): array
+    public static function NotIn(string $fieldName, array|string $values): array
     {
-        return array($field, "NOT IN", $values);
+        return array($fieldName, "NOT IN", $values);
     }
 
     /**
      *
-     * @param string $field
+     * @param string $fieldName
      * @param string $value
      * @param $align_position string 字符串$value跟目标字符串的对齐关系。取值为："left"|"right"|"middle"，缺省值为"middle"
      * @return array
      */
-    public static function Like(string $field, string $value, string $align_position = "middle"): array
+    public static function Like(string $fieldName, string $value, string $align_position = "middle"): array
     {
         if ($align_position == "left") {
             $value .= "%";
@@ -85,17 +85,17 @@ class WhereHelper
             $value = "%" . $value . "%";
         }
 
-        return array($field, "LIKE", $value);
+        return array($fieldName, "LIKE", $value);
     }
 
     /**
      *
-     * @param string $field
+     * @param string $fieldName
      * @param string $value
      * @param $align_position string 字符串$value跟目标字符串的对齐关系。取值为："left"|"right"|"middle"，缺省值为"middle"
      * @return array
      */
-    public static function NotLike(string $field, string $value, string $align_position = "middle"): array
+    public static function NotLike(string $fieldName, string $value, string $align_position = "middle"): array
     {
         if ($align_position == "left") {
             $value .= "%";
@@ -105,50 +105,69 @@ class WhereHelper
             $value = "%" . $value . "%";
         }
 
-        return array($field, "NOT LIKE", $value);
+        return array($fieldName, "NOT LIKE", $value);
+    }
+
+    /**
+     * 查询字符串集合类型的字段中是否包含指定的字符串。
+     * 比如，一个字段存储了字符串集合，如："apple,banana,orange,pear"，则可以使用该方法查询是否包含"banana"。
+     * @param string $fieldName
+     * @param string $item
+     * @param string $separator 各个子字符串之间的分隔符，缺省为","
+     * @return array where条件的二维数组，这个数组要交给ThinkORM的 whereOr() 方法使用
+     */
+    public static function LikeAtStringCollection(string $fieldName, string $item, string $separator = ","): array
+    {
+        $whereOr   = [];
+        $whereOr[] = [$fieldName, 'like', $item . $separator . '%'];
+        $whereOr[] = [$fieldName, 'like', '%' . $separator . $item . $separator . '%'];
+        $whereOr[] = [$fieldName, 'like', '%' . $separator . $item];
+        $whereOr[] = [$fieldName, '=', $item];
+
+        return $whereOr;
     }
 
     /**
      * 查询区间（包含min_value和max_value）
-     * @param string $field
+     * @param string $fieldName
      * @param $min_value
      * @param $max_value
      * @return array
      */
-    public static function Between(string $field, $min_value, $max_value): array
+    public static function Between(string $fieldName, $min_value, $max_value): array
     {
-        return array($field, "BETWEEN", array($min_value, $max_value));
+        return array($fieldName, "BETWEEN", array($min_value, $max_value));
     }
 
     /**
      * 查询区间外（不包含min_value和max_value）
-     * @param string $field
+     * @param string $fieldName
      * @param $min_value
      * @param $max_value
      * @return array
      */
-    public static function NotBetween(string $field, $min_value, $max_value): array
+    public static function NotBetween(string $fieldName, $min_value, $max_value): array
     {
-        return array($field, "NOT BETWEEN", array($min_value, $max_value));
+        return array($fieldName, "NOT BETWEEN", array($min_value, $max_value));
     }
 
     /**
      * 查询字段为NULL
-     * @param string $field
+     * @param string $fieldName
      * @return array
      */
-    public static function Null(string $field): array
+    public static function Null(string $fieldName): array
     {
-        return array($field, "NULL", NULL);
+        return array($fieldName, "NULL", NULL);
     }
 
     /**
      * 查询字段不为NULL
-     * @param string $field
+     * @param string $fieldName
      * @return array
      */
-    public static function NotNull(string $field): array
+    public static function NotNull(string $fieldName): array
     {
-        return array($field, "NOT NULL", NULL);
+        return array($fieldName, "NOT NULL", NULL);
     }
 }
